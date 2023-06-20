8c8e45b0ef9617ef2fb22826e11595831d2d9e49
adil majhoul <itsadilhadid@gmail.com>
2023-06-20 16:22:13 +0100

    ALX HIGH LEVEL

---
+++ main.c
@@ -0,0 +1,25 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include "monty.h"
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+
+    size_t line_length = 0;
+    int bytes_read = 0;
+
+    FILE *file_descriptor;
+
+    char *file_name = argv[1];
+
+    char *buffer = NULL;
+
+    file_descriptor = fopen(file_name, "r");
+
+    while ((bytes_read = getline(&buffer, &line_length, file_descriptor)) != -1)
+    {
+        printf("%s", buffer);
+    }
+
+    return (0);
+}
