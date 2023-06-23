d478de94f36fe32af06bd632193e8056dced74ad
adil majhoul <itsadilhadid@gmail.com>
2023-06-23 18:30:17 +0100

    ALX HIGH LEVEL

--- mod.c
+++ mod.c
@@ -12,16 +12,16 @@
 void mod(stack_t **stack, unsigned int line_number)
 {
 
-    if (!*stack || !(*stack)->next)
-    {
-        which_error(MOD_ERROR, NULL, line_number, NULL);
-    }
-    if ((*stack)->n == 0)
-    {
-        which_error(DIV_ZERO_ERROR, NULL, line_number, NULL);
-    }
+	if (!*stack || !(*stack)->next)
+	{
+		which_error(MOD_ERROR, NULL, line_number, NULL);
+	}
+	if ((*stack)->n == 0)
+	{
+		which_error(DIV_ZERO_ERROR, NULL, line_number, NULL);
+	}
 
-    (*stack)->next->n %= (*stack)->n;
+	(*stack)->next->n %= (*stack)->n;
 
-    pop(&head, line_number);
+	pop(&head, line_number);
 }
