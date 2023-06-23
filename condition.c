89bcf89a5f20ae78a79f9d25cbd56e41c225bd91
adil majhoul <itsadilhadid@gmail.com>
2023-06-23 03:10:11 +0100

    ALX HIGH LEVEL

--- condition.c
+++ condition.c
@@ -12,16 +12,18 @@ void conditions(char *opcode, size_t line, char *parameter, char *buffer)
 	void (*func)(stack_t **stack, unsigned int line_number);
 	char *valid_opcodes[] = {"push", "pall", "pint", "pop", "swap", "add", NULL};
 
-	if (is_all_whitespace(buffer))
+	if (is_all_whitespace(buffer) || strstr(buffer, "nop") != NULL)
 		return;
 
 	opcode = strtok(buffer, "\t\n\r\v\f ");
 	parameter = strtok(NULL, "\t\n\r\v\f ");
 
-	if (strcmp(opcode, "push") == 0 && !is_digit(parameter))
-		which_error(PUSH_ERROR, NULL, line, buffer);
 	if (!valid_opcode(opcode, valid_opcodes))
 		which_error(UNKNOWN_INSTRUCTION, opcode, line, buffer);
+
+	if (strcmp(opcode, "push") == 0 && !is_digit(parameter))
+		which_error(PUSH_ERROR, NULL, line, buffer);
+
 	if (opcode != NULL)
 	{
 		func = select_operation(opcode);
